89ef00e2084b611291a57367f646314fc09a1e03
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2020-01-21 17:56:45 -0500

    Changed primary key constraint definition to use PrimaryKeyConstraint()

--- movr/models.py
+++ movr/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Index, String, DateTime, Integer, Boolean, Float, Interval, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Index, String, DateTime, Integer, Boolean, Float, Interval, ForeignKey, CheckConstraint, PrimaryKeyConstraint
 from sqlalchemy.types import DECIMAL, DATE
 from sqlalchemy.dialects.postgresql import UUID, ARRAY
 import datetime
@@ -21,14 +21,15 @@ class User(Base, UserMixin):
         User -- Instance of the User class.
     """
     __tablename__ = 'users'
-    id = Column(UUID, primary_key=True)
-    city = Column(String, primary_key=True)
+    id = Column(UUID)
+    city = Column(String)
     first_name = Column(String)
     last_name = Column(String)
     email = Column(String)
     username = Column(String, unique=True)
     password_hash = Column(String)
     is_owner = Column(Boolean)
+    PrimaryKeyConstraint(city, id)
 
     def set_password(self, password):
         """
@@ -51,8 +52,8 @@ class Vehicle(Base):
         Vehicle -- Instance of the Vehicle class.
     """
     __tablename__ = 'vehicles'
-    id = Column(UUID, primary_key=True)
-    city = Column(String, primary_key=True)
+    id = Column(UUID)
+    city = Column(String)
     type = Column(String)
     owner_id = Column(UUID, ForeignKey('users.id'))
     date_added = Column(DATE, default=datetime.date.today)
@@ -60,6 +61,7 @@ class Vehicle(Base):
     last_location = Column(String)
     color = Column(String)
     brand = Column(String)
+    PrimaryKeyConstraint(city, id)
 
     def __repr__(self):
         return "<Vehicle(city='{0}', id='{1}', type='{2}', status='{3}')>".format(self.city, self.id, self.type, self.status)
@@ -76,8 +78,8 @@ class Ride(Base):
         Ride -- Instance of the Ride class.
     """
     __tablename__ = 'rides'
-    id = Column(UUID, primary_key=True)
-    city = Column(String, ForeignKey('vehicles.city'), primary_key=True)
+    id = Column(UUID)
+    city = Column(String, ForeignKey('vehicles.city'))
     rider_id = Column(UUID, ForeignKey('users.id'))
     rider_city = Column(String, ForeignKey('users.city'))
     vehicle_id = Column(UUID, ForeignKey('vehicles.id'))
@@ -86,6 +88,7 @@ class Ride(Base):
     start_time = Column(DateTime)
     end_time = Column(DateTime)
     length = Column(Interval)
+    PrimaryKeyConstraint(city, id)
 
     def __repr__(self):
         return "<Ride(city='{0}', id='{1}', rider_id='{2}', vehicle_id='{3}')>".format(self.city, self.id, self.rider_id, self.vehicle_id)
